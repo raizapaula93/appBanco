66474708400eeee79ca394327f01768c37e7a5dc
raizapaula93 <raizapaula93@gmail.com>
2022-03-01 17:05:34 -0300

    [TASK:MODIFY registers]

--- src/contaBanco/appBanco.java
+++ src/contaBanco/appBanco.java
@@ -5,24 +5,21 @@ public class appBanco {
 	public static void main(String[] args) {
 		CadastroConta conta1 = new CadastroConta();// instanciando o objeto
 		CadastroConta conta2 = new CadastroConta();
-		
+
+		conta1.abrirConta();
+		System.out.println("-------------");
+		conta1.setNumConta("0001");
+		conta1.setTipoConta("CC");
+		conta1.setDono("Jubileu");
 		conta1.status();
+
+		System.out.println("-------------");
+		conta2.abrirConta();
+		conta2.setNumConta("0002");
+		conta2.setTipoConta("CP");
+		conta2.setDono("Creuza");
 		System.out.println("-------------");
 		conta2.status();
-		System.out.println("-------------");
-		//conta1.abrirConta();
-		System.out.println("-------------");
-		//conta1.status();
-		//conta1.setNumConta("0001");
-		//conta1.setTipoConta("CC");
-		//conta1.setDono("Jubileu");
-		//System.out.println("-------------");
-		//conta1.status();
-		
-		
-		
-		
-		
 
 	}
 
